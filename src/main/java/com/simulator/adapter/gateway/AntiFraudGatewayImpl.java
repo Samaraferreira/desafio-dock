7a9ac5d0293cd9103fca942f67cfddb45677fd2d
Samara Ferreira <ferreirasamara.sf@gmail.com>
2025-01-05 18:52:13 -0300

    fix: add AntiFraudClient

--- src/main/java/com/simulator/adapter/gateway/AntiFraudGatewayImpl.java
+++ src/main/java/com/simulator/adapter/gateway/AntiFraudGatewayImpl.java
@@ -2,6 +2,7 @@ package com.simulator.adapter.gateway;
 
 import com.simulator.core.gateway.AntiFraudGateway;
 import com.simulator.infrastructure.rest.antifraud.AntiFraudClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
@@ -10,6 +11,10 @@ import java.math.BigDecimal;
 public class AntiFraudGatewayImpl implements AntiFraudGateway {
 
     private AntiFraudClient antiFraudClient;
+
+    public AntiFraudGatewayImpl(@Autowired AntiFraudClient antiFraudClient) {
+        this.antiFraudClient = antiFraudClient;
+    }
 
     @Override
     public boolean validate(String clientId, BigDecimal amount) {
